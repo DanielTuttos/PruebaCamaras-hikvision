77e2d795a067e7d531901455d4ebf43a8004518d
DanielTuttos <tutorialesdanielromero@gmail.com>
2020-12-01 15:06:09 -0500

    Ya esta funcionando la libreria

--- src/Vistas/Camaras.java
+++ src/Vistas/Camaras.java
@@ -1,30 +1,32 @@
 package Vistas;
 
-import com.hikvision.netsdk.HCNetSDK;
-import com.hikvision.netsdk.INT_PTR;
-import com.hikvision.netsdk.NET_DVR_DEVICEINFO_V30;
-import com.hikvision.netsdk.NET_DVR_PREVIEWINFO;
-import com.hikvision.netsdk.RealPlayCallBack;
-import com.hikvision.netsdk.NET_DVR_JPEGPARA;
+
+//import com.hikvision.netsdk.INT_PTR;
+//import com.hikvision.netsdk.NET_DVR_PREVIEWINFO;
+//import com.hikvision.netsdk.RealPlayCallBack;
+//import com.hikvision.netsdk.NET_DVR_JPEGPARA;
+
+import com.sun.jna.NativeLong;
 
 import javax.swing.*;
 
 
 public class Camaras extends JFrame {
     private JPanel pane1;
-    private JPanel realPlayPane;
+    private JPanel RealPlayPane;
+
+    private static HCNetSDK netSdkInstance = HCNetSDK.INSTANCE;
 
 
-    private final HCNetSDK netSdkInstance = HCNetSDK.getInstance();
-    private final NET_DVR_DEVICEINFO_V30 deviceInfo30 = new NET_DVR_DEVICEINFO_V30();
+    HCNetSDK.NET_DVR_DEVICEINFO_V30 deviceInfo30 ;
 
 
     public String ipAddress = "192.168.0.102";
     public String userName = "admin";
     public String password = "Temporal2020";
-    public Integer port = 8000;
+    public String port = "8000";
 
-    int loginId = -1;
+    NativeLong loginId = new NativeLong(-1);
     private int realPlayId = -1; // return by NET_DVR_RealPlay_V30
     private int playbackId = -1; // return by NET_DVR_PlayBackByTime
     private int playPort = -1; // play port
@@ -32,7 +34,7 @@ public class Camaras extends JFrame {
     private boolean stopPlayback = false;
     private boolean isShow = true;
 
-    private INT_PTR error;
+    // private INT_PTR error;
 
     public Camaras(){
         super("Camaras");
@@ -41,14 +43,17 @@ public class Camaras extends JFrame {
     }
 
     public void login(){
-        loginId = netSdkInstance.NET_DVR_Login_V30(ipAddress, port, userName, password, deviceInfo30);
+
+        System.out.println("Creamos la instancia "+ netSdkInstance);
+        int iPort = Integer.parseInt(port);
+        loginId = netSdkInstance.NET_DVR_Login_V30(ipAddress,(short) iPort, userName, password, deviceInfo30);
         System.out.println("Id camaras: "+ loginId);
-        if (loginId < 0) {
-            error = new INT_PTR();
-            error.iValue = netSdkInstance.NET_DVR_GetLastError();
-            System.out.println("NET_DVR_Login is failed!Err: "
-                    + netSdkInstance.NET_DVR_GetErrorMsg(error));
-        }
+        //if (loginId < 0) {
+        //    error = new INT_PTR();
+        //    error.iValue = netSdkInstance.NET_DVR_GetLastError();
+        //    System.out.println("NET_DVR_Login is failed!Err: "
+        //            + netSdkInstance.NET_DVR_GetErrorMsg(error));
+        //}
 
     }
 }
